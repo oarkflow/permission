eae41597b856fd2f460d69c5dcea73486b54809f
sujit <s.baniya.np@gmail.com>
2024-04-24 13:55:08 +0545

    feat: changed `route` to `page` to indicate frontend group

--- examples/main.go
+++ examples/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	
+
 	"github.com/oarkflow/permission"
 )
 
@@ -11,29 +11,29 @@ func main() {
 	module := permission.NewModule("ModuleA")
 	company.AddModule(module)
 	// company.SetDefaultModule(module.ID)
-	
+
 	coder, qa, suspendManager, _ := myRoles()
 	company.AddRole(coder, qa, suspendManager)
-	
+
 	e29 := permission.NewEntity("29")
 	e30 := permission.NewEntity("30")
 	e33 := permission.NewEntity("33")
-	
+
 	company.AddEntities(e29, e30, e33)
-	
+
 	userA := permission.NewUser("userA")
 	userB := permission.NewUser("userB")
 	userC := permission.NewUser("userC")
-	
+
 	company.AddUser(userA.ID, coder.ID)
 	company.AddUser(userB.ID, qa.ID)
 	company.AddUser(userC.ID, suspendManager.ID)
-	
+
 	company.AssignEntitiesToUser(userA.ID, e29.ID)
 	company.AssignEntitiesToUser(userB.ID, e30.ID)
 	company.AssignEntitiesToUser(userC.ID, e33.ID)
-	fmt.Println("R:", permission.Can(userA.ID, "CompanyA", "ModuleA", e29.ID, "route", "/coding/1/2/start-coding POST"), "E:", true)
-	fmt.Println("R:", permission.Can(userA.ID, "CompanyA", "ModuleA", e29.ID, "route", "/coding/1/open GET"), "E:", true)
+	fmt.Println("R:", permission.Can(userA.ID, "CompanyA", "ModuleA", e29.ID, "page", "/coding/1/2/start-coding POST"), "E:", true)
+	fmt.Println("R:", permission.Can(userA.ID, "CompanyA", "ModuleA", e29.ID, "page", "/coding/1/open GET"), "E:", true)
 	fmt.Println("R:", permission.Can(userA.ID, "CompanyA", "ModuleA", e29.ID, "backend", "/coding/1/2/start-coding POST"), "E:", false)
 }
 
@@ -45,8 +45,8 @@ func myRoles() (coder *permission.Role, qa *permission.Role, suspendManager *per
 		{"/coding/:wid/in-progress", "GET"},
 		{"/coding/:wid/:eid/review", "POST"},
 	}
-	coder.AddPermission("route", perm...)
-	
+	coder.AddPermission("page", perm...)
+
 	qa = permission.NewRole("qa")
 	perm = []permission.Attribute{
 		{"/coding/:wid/:eid/start-qa", "POST"},
@@ -54,22 +54,22 @@ func myRoles() (coder *permission.Role, qa *permission.Role, suspendManager *per
 		{"/coding/:wid/qa-in-progress", "GET"},
 		{"/coding/:wid/:eid/qa-review", "POST"},
 	}
-	qa.AddPermission("route", perm...)
-	
+	qa.AddPermission("page", perm...)
+
 	suspendManager = permission.NewRole("suspend-manager")
 	perm = []permission.Attribute{
 		{"/coding/:wid/suspended", "GET"},
 		{"/coding/:wid/:eid/release-suspend", "POST"},
 		{"/coding/:wid/:eid/request-abandon", "POST"},
 	}
-	suspendManager.AddPermission("route", perm...)
-	
+	suspendManager.AddPermission("page", perm...)
+
 	admin = permission.NewRole("admin")
 	perm = []permission.Attribute{
 		{"/admin/user/add", "POST"},
 		{"/admin/user/edit", "PUT"},
 	}
-	admin.AddPermission("route", perm...)
+	admin.AddPermission("page", perm...)
 	admin.AddDescendent(coder, qa, suspendManager)
 	return
 }
