684ea61b910646e23eed74619457955a76037b55
sujit <s.baniya.np@gmail.com>
2024-06-30 17:58:26 +0545

    update: add cache in role_manager

--- trie/trie.go
+++ trie/trie.go
@@ -52,23 +52,12 @@ func New[T DataProps](match SearchFunc[T]) *Trie[T] {
 	}
 }
 
-func (t *Trie[T]) search(filter *T, callback SearchFunc[T]) []*T {
+func (t *Trie[T]) search(filter *T, callback SearchFunc[T], first ...bool) []*T {
 	results := t.dataSlice.Get().(*[]*T)
 	defer t.dataSlice.Put(results)
 	*results = (*results)[:0]
-	t.searchRecursiveFunc(t.root, filter, callback, results)
+	t.searchIterative(filter, callback, results, first...)
 	return *results
-}
-
-func (t *Trie[T]) first(filter *T, callback SearchFunc[T]) *T {
-	results := t.dataSlice.Get().(*[]*T)
-	defer t.dataSlice.Put(results)
-	*results = (*results)[:0]
-	t.firstRecursiveFunc(t.root, filter, callback, results)
-	if len(*results) > 0 {
-		return (*results)[0]
-	}
-	return nil
 }
 
 func (t *Trie[T]) Data() []*T {
@@ -110,6 +99,14 @@ func (t *Trie[T]) FirstFunc(filter *T, callback SearchFunc[T]) *T {
 	return t.first(filter, callback)
 }
 
+func (t *Trie[T]) first(filter *T, callback SearchFunc[T]) *T {
+	rs := t.search(filter, callback)
+	if len(rs) > 0 {
+		return rs[0]
+	}
+	return nil
+}
+
 func (t *Trie[T]) Search(filter *T) []*T {
 	return t.search(filter, t.match)
 }
@@ -118,43 +115,19 @@ func (t *Trie[T]) SearchFunc(filter *T, callback SearchFunc[T]) []*T {
 	return t.search(filter, callback)
 }
 
-func (t *Trie[T]) searchRecursiveFunc(node *Node[T], filter *T, callback SearchFunc[T], results *[]*T) {
-	if node.isEnd && callback(filter, node.data) {
-		*results = append(*results, node.data)
-	}
-	for _, child := range node.child {
-		t.searchRecursiveFunc(child, filter, callback, results)
-	}
-}
-
-func (t *Trie[T]) firstRecursiveFunc(node *Node[T], filter *T, callback SearchFunc[T], results *[]*T) {
-	if node.isEnd && callback(filter, node.data) {
-		*results = append(*results, node.data)
-	}
-	if len(*results) == 1 {
-		return
-	}
-	for _, child := range node.child {
-		t.firstRecursiveFunc(child, filter, callback, results)
-		if len(*results) == 1 {
-			break
+func (t *Trie[T]) searchIterative(filter *T, callback SearchFunc[T], results *[]*T, first ...bool) {
+	stack := []*Node[T]{t.root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node.isEnd && callback(filter, node.data) {
+			*results = append(*results, node.data)
+		}
+		if len(first) > 0 && first[0] && len(*results) == 1 {
+			return
+		}
+		for _, child := range node.child {
+			stack = append(stack, child)
 		}
 	}
-}
-
-func IsNil(value any) bool {
-	return value == nil
-}
-
-func MatchesFilter(value, filter any) bool {
-	return !IsNil(filter) && value == filter
-}
-
-func FilterByFields[T DataProps](filter *T, row *T, fields ...func(*T) any) bool {
-	for _, field := range fields {
-		if IsNil(field(row)) || !MatchesFilter(field(row), field(filter)) {
-			return false
-		}
-	}
-	return true
 }
